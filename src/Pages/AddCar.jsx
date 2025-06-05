8b5584dbfba005303d1e2f2464031b7ce0606efb
Md-Shakib-Akando <shakibakando2@gmail.com>
2025-06-05 14:14:21 +0600

    add cars data in database

--- src/Pages/AddCar.jsx
+++ src/Pages/AddCar.jsx
@@ -1,13 +1,61 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../AuthContext';
+import Swal from 'sweetalert2';
 
 const AddCar = () => {
+    const { user } = useContext(AuthContext)
+    const handleAddCars = e => {
+        e.preventDefault();
+
+        const form = e.target;
+        const formData = new FormData(form);
+        const carData = Object.fromEntries(formData.entries());
+
+        const fullData = {
+            ...carData,
+            userName: user.displayName,
+            userEmail: user.email,
+            bookingCount: 0,
+
+
+        };
+        fetch('http://localhost:3000/cars', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(fullData),
+        })
+         .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          Swal.fire({
+            icon: 'success',
+            title: 'Car Added Successfully',
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        //   form.reset();
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        Swal.fire({
+          icon: 'error',
+          title: 'Failed to Add Car',
+          text: 'Please try again later.',
+        });
+      });
+
+
+    }
     return (
         <>
             <div className='bg-orange-50'>
                 <div className='  max-w-11/12 mx-auto min-h-[calc(100vh-845px)] py-10'>
                     <div className="max-w-xl md:max-w-2xl xl:max-w-6xl mx-auto  bg-blue-50 p-8 rounded-md shadow-md">
                         <h2 className="text-2xl font-bold text-center mb-6">Add New Car</h2>
-                        <form className="space-y-4">
+                        <form onSubmit={handleAddCars} className="space-y-4">
                             <div>
                                 <label className="block mb-1">Car Model</label>
                                 <input type="text" name="carModel" className="w-full p-2 border border-gray-300 outline-0  rounded" placeholder="Enter car model" required />
@@ -51,12 +99,12 @@ const AddCar = () => {
                                 <input type="text" name="location" className="w-full p-2 border  border-gray-300 outline-0 rounded" placeholder="Enter location" required />
                             </div>
 
-                            
+
                             <button
                                 type="submit"
                                 className="w-full bg-gradient-to-r from-orange-500 to-yellow-400 hover:from-orange-600 hover:to-yellow-500 text-white font-bold py-3 rounded-lg shadow-lg hover:shadow-xl transition duration-300"
                             >
-                               Add Car
+                                Add Car
                             </button>
                         </form>
                     </div>
