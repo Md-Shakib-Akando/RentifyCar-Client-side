2879d20974c36abd74f012488b433f55fbd3e9fe
Md-Shakib-Akando <mdshakibss80792@gmail.com>
2025-08-10 18:05:01 +0600

    home section card add see more btn

--- src/Pages/RecentCars.jsx
+++ src/Pages/RecentCars.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { formatDistanceToNow } from 'date-fns';
-import { FaCar} from 'react-icons/fa';
+import { FaCar } from 'react-icons/fa';
 import { FaSackDollar } from 'react-icons/fa6';
 import { motion } from 'framer-motion';
 import { HiBookmark, HiCheckCircle, HiClock, HiMiniXCircle } from 'react-icons/hi2';
+import { Link } from 'react-router';
 const RecentCars = ({ car }) => {
-    const { _id, createdAt, bookingCount, imageUrl, availability, dailyRentalPrice, carModel } = car;
+    const { _id, createdAt, bookingCount, imageUrl, availability, dailyRentalPrice, carModel,
+        description } = car;
     const postedAgo = createdAt
         ? `Added ${formatDistanceToNow(new Date(createdAt), { addSuffix: true })}`
         : 'N/A';
@@ -15,7 +17,7 @@ const RecentCars = ({ car }) => {
         <motion.div
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, ease: 'easeOut', delay:0.2 }}
+            transition={{ duration: 1, ease: 'easeOut', delay: 0.2 }}
             viewport={{ once: true }}
             className=" rounded-md shadow-md border-1 border-orange-300  dark:bg-gray-50 dark:text-gray-800 transition-transform transform hover:scale-105 hover:shadow-md hover:shadow-amber-50 hover:cursor-pointer">
 
@@ -27,7 +29,7 @@ const RecentCars = ({ car }) => {
 
                         <span className='flex'><h1 className='flex items-center gap-3 text-xl font-semibold'><FaCar className='text-orange-400' size={24}></FaCar>{carModel}</h1></span>
                         <span className='mt-2 mb-1'><h1 className=' flex items-center gap-4 font-medium'><FaSackDollar className='text-orange-400' size={16}></FaSackDollar>Price : ${dailyRentalPrice}/day</h1></span>
-                        <span className='mb-1'><h1 className=' font-medium'>{availability=="Available" ? (
+                        <span className='mb-1'><h1 className=' font-medium'>{availability == "Available" ? (
                             <>
                                 <span className='flex items-center gap-2'><HiCheckCircle size={20} className='text-green-400'></HiCheckCircle>Available</span>
                             </>
@@ -38,6 +40,26 @@ const RecentCars = ({ car }) => {
                         )}</h1></span>
                         <span className='mb-1'><h1 className=' flex items-center gap-2 font-medium'><HiBookmark className='text-blue-400' size={20}></HiBookmark>BookingCount: {bookingCount}</h1></span>
                         <span className='mb-3'><h1 className=' flex items-center gap-2 font-medium'><HiClock className='text-pink-400' size={20}></HiClock>Date Posted: {postedAgo}</h1></span>
+                        {description && (
+                            <div>
+
+                                <p className="text-sm text-gray-600 dark:text-gray-700 line-clamp-2">
+                                    {description}
+                                </p>
+
+
+                                <div className='flex justify-end mr-3'>
+                                    <Link
+                                        to={`/carDetails/${_id}`}
+                                        className="inline-block mt-1 text-orange-500 font-medium hover:underline"
+                                    >
+                                        See more
+                                    </Link>
+                                </div>
+                            </div>
+                        )}
+
+
                     </div>
                 </div>
 
